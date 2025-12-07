4883fc71df618e3c38b2b2ed237af041790eddf9
lucascorreia-tech <lucasrcorreia23@gmail.com>
2025-12-06 22:58:59 -0300

    "Primeiro Projeto"

---
+++ 1. Projetos Inciantes/Calculadora/Program.cs
@@ -0,0 +1,36 @@
+﻿Console.WriteLine("Bem vindo à Calculadora!");
+
+Console.WriteLine("Digite o primeiro número:");
+string? numero1 = Console.ReadLine();
+
+Console.WriteLine("Digite o segundo número:");
+string? numero2 = Console.ReadLine();
+
+Console.WriteLine("Qual operação vai ser ?\n + -> Soma\n - -> Diferença\n * -> Multiplicação\n / -> Divisão");
+string? opção = Console.ReadLine();
+
+double resultado = 0;
+
+switch(opção)
+{
+  case "+":
+    resultado = double.Parse(numero1) + double.Parse(numero2);
+    Console.WriteLine($"Resultado é {resultado}");
+    break;
+  case "-":
+    resultado = double.Parse(numero1) - double.Parse(numero2);
+    Console.WriteLine($"Resultado é {resultado}");
+    break;
+  case "*":
+    resultado = double.Parse(numero1) * double.Parse(numero2);
+    Console.WriteLine($"Resultado é {resultado}");
+    break;
+  case "/":
+    resultado = double.Parse(numero1) / double.Parse(numero2);
+    Console.WriteLine($"Resultado é {resultado}");
+    break;
+  default:
+    Console.WriteLine("Opção não existe.");
+    break;
+}
+  
