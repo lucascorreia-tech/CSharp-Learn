b9c4424cd8b117c8a26f3a823b036912dd82b431
lucascorreia-tech <lucasrcorreia23@gmail.com>
2025-12-07 21:28:12 -0300

    "Segundo projeto"

---
+++ 1. Projetos Inciantes/Lista_Convidados/Program.cs
@@ -0,0 +1,38 @@
+﻿Console.WriteLine("Escolha uma das opções abaixo");
+string opcao;
+List<string> convidados = [];
+
+do{
+  Console.Clear();
+  Console.WriteLine("1. Adicionar Convidado\n 2. Remover nome\n 3. Listar todos\n 4. Sair do programa");
+  opcao = Console.ReadLine()!;
+
+  switch (opcao)
+  {
+    case "1":
+      Console.WriteLine("Digite o nome do convidado: ");
+      string? nomeConvidado = Console.ReadLine();
+      convidados.Add(nomeConvidado!);
+      break;
+    case "2":
+      Console.WriteLine("Digite o nome do convidado que quer remover: ");
+      string? nomeConvidadoRemocao = Console.ReadLine();
+      convidados.Remove(nomeConvidadoRemocao!);
+      break;
+    case "3":
+      Console.WriteLine("Todos os convidados");
+      foreach (string convidado in convidados)
+      {
+        Console.WriteLine(convidado);
+      }
+      Console.ReadKey();
+      break;
+    case "4":
+      Console.WriteLine("Saindo... ");
+      break;
+    default:
+      Console.WriteLine("Nenhuma opção marcada");
+      break;
+  }
+} while (opcao != "4");
+
