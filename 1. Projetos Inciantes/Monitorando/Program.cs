840f3e1d78e0def5d7594e8ea3b4a6837ae1edfa
Lucas R Correia <lucasrcorreia23@gmail.com>
2025-12-14 22:32:21 -0300

    "Novo desafio"

---
+++ 1. Projetos Inciantes/Monitorando/Program.cs
@@ -0,0 +1,14 @@
+﻿
+static async Task DownloadArquivoAsync(string nome, int tempo)
+{
+    await Task.Delay(tempo);
+    Console.WriteLine($"Download do arquivo com nome: {nome} foi concluido");
+}
+
+var taskNotion = DownloadArquivoAsync("Notion", 3000); 
+var taskFirefox = DownloadArquivoAsync("Firefox", 4000);
+var taskChrome = DownloadArquivoAsync("Chrome", 2000);
+
+await Task.WhenAll(taskNotion, taskChrome, taskFirefox);
+
+Console.WriteLine("Todas foram concluidas");
